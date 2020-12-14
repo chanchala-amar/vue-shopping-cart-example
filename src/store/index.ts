160aff6a61b6e98567c7dad8fc9616aafe4415c0
C Murthy <chanchala.amar@gmail.com>
2020-12-14 23:04:08 +0530

    app initial state with basic store functionality

--- src/store/index.ts
+++ src/store/index.ts
@@ -4,8 +4,61 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    products: [
+      {
+        id: 1,
+        name: "Product1",
+        image: "../assets/apple_raw.png",
+        price: 10
+      },
+      {
+        id: 2,
+        name: "Product2",
+        image: "../assets/apple_raw.png",
+        price: 20
+      },
+      {
+        id: 3,
+        name: "Product3",
+        image: "../assets/apple_raw.png",
+        price: 30
+      },
+      {
+        id: 4,
+        name: "Product4",
+        image: "../assets/apple_raw.png",
+        price: 40
+      },
+      {
+        id: 5,
+        name: "Product5",
+        image: "../assets/apple_raw.png",
+        price: 50
+      }
+    ],
+    storeCart: [] as number[],
+  },
+  getters: {
+    getProducts: state => state.products,
+    getStoreCart: state => state.storeCart
+  },
+  mutations: {
+    ADD_CART_ITEM(state, id) {
+      state.storeCart.push(0);
+      state.storeCart.push(id);
+    },
+    REMOVE_CART_ITEM(state, index) {
+      state.storeCart.splice(index, 1);
+    }
+  },
+  actions: {
+    addProduct(context, id) {
+      context.commit("ADD_CART_ITEM", id);
+    },
+    removeProduct(context, id) {
+      context.commit("REMOVE_CART_ITEM", id);
+    }
+  },
   modules: {}
 });
